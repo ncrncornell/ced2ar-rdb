819f8ee8a8f161ba4d283e9aa8c3b8a7ebbab883
Kyle Brumsted <kyle.brumsted@mail.mcgill.ca>
2017-01-18 11:03:53 -0500

    added function to get list of all vars in db

--- rdb/src/main/java/edu/ncrn/cornell/model/dao/FieldInstDao.java
+++ rdb/src/main/java/edu/ncrn/cornell/model/dao/FieldInstDao.java
@@ -8,4 +8,6 @@ public interface FieldInstDao extends BaseRepository<FieldInst, Long> {
 	public List<FieldInst> findByRawDocIdAndFieldId(String rawDocId, String fieldId);
 	public List<FieldInst> findByRawDocIdAndCanonicalXpath(String rawDocId, String canonicalXpath);
 	public List<FieldInst> findByRawDocIdAndValue(String rawDocId, String value);
+	public List<FieldInst> findByFieldId(String fieldId);
+	public List<FieldInst> findByCanonicalXpath(String canonicalXpth);
 }
