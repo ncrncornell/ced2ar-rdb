043c999ac80bbbf1c68ecedd8d9601a77ab46f31
Kyle Brumsted <kyle.brumsted@mail.mcgill.ca>
2017-02-07 11:34:36 -0500

    refactored details service return types, added pagination calls for var list services

--- rdb/src/main/java/edu/ncrn/cornell/model/dao/FieldInstDao.java
+++ rdb/src/main/java/edu/ncrn/cornell/model/dao/FieldInstDao.java
@@ -2,12 +2,19 @@ package edu.ncrn.cornell.model.dao;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import edu.ncrn.cornell.model.FieldInst;
 
-public interface FieldInstDao extends BaseRepository<FieldInst, Long> {
+public interface FieldInstDao extends JpaRepository<FieldInst, Long> {
 	public List<FieldInst> findByRawDocIdAndFieldId(String rawDocId, String fieldId);
 	public List<FieldInst> findByRawDocIdAndCanonicalXpath(String rawDocId, String canonicalXpath);
 	public List<FieldInst> findByRawDocIdAndValue(String rawDocId, String value);
 	public List<FieldInst> findByFieldId(String fieldId);
+	public Page<FieldInst> findByFieldId(String fieldId, Pageable pageAble);
+	public List<FieldInst> findByFieldIdAndRawDocIdIn(String fieldId, List<String> rawDocIdList);
+	public Page<FieldInst> findByFieldIdAndRawDocIdIn(String fieldId, List<String> rawDocIdList, Pageable pageable);
 	public List<FieldInst> findByCanonicalXpath(String canonicalXpth);
 }
